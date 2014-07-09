dcd14e51fabf13489f432067f4bb64e2b9a11079
Jing Wu <aimjwizards@gmail.com>
2014-07-09 14:27:06 -0400

    added greetByPeriod(name, period) function 1. index.html need to add js 2. hello.js needs to add java support 3. update java functionality.

--- Lesson_2/000_Hello_Endpoints/src/main/java/com/google/training/helloworld/HelloClass.java
+++ Lesson_2/000_Hello_Endpoints/src/main/java/com/google/training/helloworld/HelloClass.java
@@ -1,15 +1,18 @@
 package com.google.training.helloworld;
 
 public class HelloClass {
-    public String message = "Hello World";
+    public String message = "Are you hungry?";
 
     public HelloClass () {
     }
 
     public HelloClass (String name) {
-        this.message = "Hello " + name + "!";
+        this.message = "Are you hungry? " + name + "!";
     }
 
+    public HelloClass (String name, String period) {
+    	this.message = period+ "! " + "Are you hungry? " + name+"!";
+    }
     public String getMessage() {
         return message;
     }
